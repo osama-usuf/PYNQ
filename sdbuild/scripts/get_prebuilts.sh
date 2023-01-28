a19357ea7f170f169c986f7f51079e9a5664ef09
adamlab <osamayousuf@gwu.edu>
2023-01-27 20:56:30 -0500

    updated get_prebuilts script

--- sdbuild/scripts/get_prebuilts.sh
+++ sdbuild/scripts/get_prebuilts.sh
@@ -4,6 +4,5 @@ wget https://bit.ly/pynq_arm_v3_1
 
 # For rebuilding all SD cards, both arm and aarch64 root filesystems
 # may be required depending on boards being targetted.
-mv pynq_sdist_v3_0_1 prebuilt/pynq_sdist
-mv pynq_arm_v3_1 prebuilt/pynq_rootfs.arm
-
+mv pynq_sdist_v3_0_1 prebuilt/pynq_sdist.tar.gz
+mv pynq_arm_v3_1 prebuilt/pynq_rootfs.arm.tar.gz
